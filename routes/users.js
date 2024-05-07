74e84d947e1fa1b180c336c19ac6af98e4c197e5
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-08 01:40:30 +0300

    Access query params

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const users = [];
-
-router.get("/new", (_req, res) => {
-  res.render("users/new", { defaultFistName: "Lexa" });
-});
-
-router.post("/", (req, res) => {
-  const fistName = req.body.fistName; // Access the value of the input with the name "fistName", by using the urlencoded middleware in server.js
-  if (!fistName || fistName.length < 3) {
-    res.status(500).render("users/new", { defaultFistName: fistName }); // Redirect to the form page if the input is invalid
-  } else {
-    users.push(fistName);
-    res.redirect(`users/${users.length - 1}`); // Redirect to the new route if the input is valid
-  }
-});
-
-router.get("/:id", (req, res) => {
-  res.send("Hi, " + users[req.params.id]);
+router.get("/", (req, res) => {
+  const name = req.query.name; // get the name query parameter from the url (e.g. /users?name=John)
+  res.send(`Hello, ${name}!`);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,12 +3,6 @@ console.log("Server is running...");
 const express = require("express");
 const app = express();
 
-// app.use(express.json()); // Parse JSON bodies (as sent by API clients)
-
-app.use(express.urlencoded({ extended: true })); // Parse JSON bodies (as sent by HTML forms)
-
-app.set("view engine", "ejs"); // Set the view engine to ejs
-
 app.get("/", (_req, res) => {
   res.send("Hello, Express!");
 });
