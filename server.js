91f28b30de32a2352b8e99f8077af598f11c08c1
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-07 20:04:38 +0300

    General response examples

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ console.log("Server is running...");
 const express = require("express");
 const app = express();
 
-app.get("/", (_req, res, _next) => {
-  console.log("GET request received!");
-  res.send("Hello World!");
+app.get("/", (_req, res) => {
+  // res.send("Hello World!"); // Send plain text response
+  // res.sendStatus(500); // Send status with default message
+  // res.status(500).json({ error: "Internal Error" }); // Send status with custom message
+  // res.download("./server.js"); // Download file
+  res.json({ message: "Hello World" }); // Send JSON response with default success status 200
 });
 
 app.listen(3000);
