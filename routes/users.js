e04b762962cee3f1827b5e298dbc780d12ca9435
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-07 23:21:51 +0300

    Refactor user routes to use route chaining

--- routes/users.js
+++ routes/users.js
@@ -5,16 +5,16 @@ router.post("/new", (_req, res) => {
   res.send(`Create, user!`);
 });
 
-router.get("/:id", (req, res) => {
-  res.send(`Get, user ${req.params.id}!`);
-});
-
-router.put("/:id", (req, res) => {
-  res.send(`Edit, user ${req.params.id}!`);
-});
-
-router.delete("/:id", (req, res) => {
-  res.send(`Delete, user ${req.params.id}!`);
-});
+router
+  .route("/:id")
+  .get((req, res) => {
+    res.send(`Get, user ${req.params.id}!`);
+  })
+  .put((req, res) => {
+    res.send(`Edit, user ${req.params.id}!`);
+  })
+  .delete((req, res) => {
+    res.send(`Delete, user ${req.params.id}!`);
+  });
 
 module.exports = router;
