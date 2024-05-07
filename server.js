813f1bd75c84f2052d01dca6d8adbc192917b828
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-07 20:17:05 +0300

    Render embedded html file

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ console.log("Server is running...");
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (_req, res) => {
-  // res.send("Hello World!"); // Send plain text response
-  // res.sendStatus(500); // Send status with default message
-  // res.status(500).json({ error: "Internal Error" }); // Send status with custom message
-  // res.download("./server.js"); // Download file
-  res.json({ message: "Hello World" }); // Send JSON response with default success status 200
+  res.render("index", { text: "World" });
 });
 
 app.listen(3000);
