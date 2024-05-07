1233bc6599ec2dacc6ea8f53a63aab571a83544c
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-07 23:20:55 +0300

    Add route method examples

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (_req, res) => {
-  res.send("Hello, users!");
-});
-
-router.get("/new", (_req, res) => {
-  res.send("Hello, new user!");
+router.post("/new", (_req, res) => {
+  res.send(`Create, user!`);
 });
 
 router.get("/:id", (req, res) => {
-  res.send(`Hello, user ${req.params.id}!`);
+  res.send(`Get, user ${req.params.id}!`);
+});
+
+router.put("/:id", (req, res) => {
+  res.send(`Edit, user ${req.params.id}!`);
+});
+
+router.delete("/:id", (req, res) => {
+  res.send(`Delete, user ${req.params.id}!`);
 });
 
 module.exports = router;
