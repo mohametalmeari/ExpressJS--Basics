70bd964aead899ed3cc390cc88c02708d3b23e23
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-07 20:25:53 +0300

    Add user routes

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ console.log("Server is running...");
 const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
+app.get("/users", (_req, res) => {
+  res.send("Hello, users!");
+});
+
+app.get("/users/new", (_req, res) => {
+  res.send("Hello, new user!");
+});
+
+app.get("/users/:id", (req, res) => {
+  res.send(`Hello, user ${req.params.id}!`);
+});
 
 app.listen(3000);
