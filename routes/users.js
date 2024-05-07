a577c05436697c37240c7f8aacca446f7fcf199b
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-08 00:01:03 +0300

    Add logger middleware

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/new", (_req, res) => {
-  res.send(`Create, user!`);
+const users = [{ name: "John" }, { name: "Jane" }, { name: "Jim" }];
+router.route("/").get((_req, res) => {
+  res.json(users);
 });
 
 router.route("/:id").get((req, res) => {
-  res.json(req.user); //4. Send the user object that was added to the request objects
-});
-
-const user = [{ name: "Tina" }, { name: "Tom" }, { name: "Trevor" }];
-router.param("id", (req, res, next, id) => {
-  req.user = user[id]; //1. Add the user object to the request object
-  if (!req.user) {
-    return res.status(404).send("User not found"); //2. Send an error response if the user is not found
+  if (!users[req.params.id]) {
+    res.status(404).json({ message: "User not found" });
   }
-  next(); //3. Pass control to the next handler if the user is found
+  res.json(users[req.params.id]);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,16 @@ console.log("Server is running...");
 const express = require("express");
 const app = express();
 
+const logger = (req, _res, next) => {
+  console.log(req.originalUrl);
+  next();
+};
+app.use(logger); // Add logger middleware, which runs before any request defined below this line
+
+app.get("/", (_req, res) => {
+  res.send("Hello, Express!");
+});
+
 const usersRouter = require("./routes/users");
 app.use("/users", usersRouter);
 
