d0ed4a57f61022b8e152fa06a9aadc8bf38744c5
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-07 23:29:09 +0300

    Add param middleware

--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,18 @@ router.post("/new", (_req, res) => {
 router
   .route("/:id")
   .get((req, res) => {
-    res.send(`Get, user ${req.params.id}!`);
+    res.send(`Get, user ${req.params.id}!`); //2. Send a response after next() is called
   })
   .put((req, res) => {
-    res.send(`Edit, user ${req.params.id}!`);
+    res.send(`Edit, user ${req.params.id}!`); //2. Send a response after next() is called
   })
   .delete((req, res) => {
-    res.send(`Delete, user ${req.params.id}!`);
+    res.send(`Delete, user ${req.params.id}!`); //2. Send a response after next() is called
   });
+
+router.param("id", (_req, _res, next, id) => { // Middleware function, called when the :id parameter is present in the URL
+  console.log(`User ID is ${id}`);
+  next(); //1. Pass control to the next handler
+});
 
 module.exports = router;
